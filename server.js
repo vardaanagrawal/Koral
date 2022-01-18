d72100fd29bcc098e277b272529352f5e347e3ec
Vardaan <vardaanagrawal517@gmail.com>
2022-01-18 14:31:24 +0530

    preparing to deploy

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require('dotenv');
-const path = require('path');
+const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -10,9 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //connect to mongodb
-mongoose.connect(process.env.mongo_url,
-    ()=>{console.log('connected to mongoose')}
-);
+mongoose.connect(process.env.mongo_url, () => {
+  console.log("connected to mongoose");
+});
 
 //data schema
 const itemSchema = {
@@ -20,32 +20,37 @@ const itemSchema = {
   email: String,
   subject: String,
   service: String,
-  message: String
+  message: String,
 };
 
 //data model
 const Item = mongoose.model("messages", itemSchema);
 
-
-
 //add item in todo list post request
 app.post("/addItem", async (req, res) => {
   const newItem = new Item({
-      name: req.body.name,
-      email: req.body.email,
-      subject: req.body.subject,
-      service: req.body.service,
-      message: req.body.message,
+    name: req.body.name,
+    email: req.body.email,
+    subject: req.body.subject,
+    service: req.body.service,
+    message: req.body.message,
   });
-  newItem.save()
-  .then(res.send('Message sent successfully.'))
+  newItem.save().then(res.send("Message sent successfully."));
 });
 
-app.get('/',(req,res)=>{
-    res.send('API running...')
-})
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,'/client/build')));
 
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.send("API running...");
+  });
+}
 
 app.listen(process.env.PORT, function () {
-  console.log("Express is running on port: "+process.env.PORT);
+  console.log("Express is running on port: " + process.env.PORT);
 });
